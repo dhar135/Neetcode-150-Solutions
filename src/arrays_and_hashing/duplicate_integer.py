85ff80b69614f29a793b674481aad2347d2d5e0a
Donovan Harrison <donjuan0802@gmail.com>
2024-11-01 00:19:31 -0500

    Solution: Duplicate Integer

---
+++ src/arrays_and_hashing/duplicate_integer.py
@@ -0,0 +1,38 @@
+"""
+Problem: Duplicate Integer
+Difficulty: Easy
+Link: https://neetcode.io/problems/duplicate-integer
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+Output: false
+
+Approach:
+- Create a set.
+- Loop through the array and check if the number is in the map.
+- If the element appears more than once, return true.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+"""
+from typing import List
+
+
+def has_duplicate(nums: List[int]) -> bool:
+
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
---
+++ tests/test_duplicate_integer.py
@@ -0,0 +1,6 @@
+from src.arrays_and_hashing.duplicate_integer import has_duplicate
+
+def test_has_duplicate():
+    assert has_duplicate([1, 2, 3, 3]) == True
+
+    assert has_duplicate([1, 2, 3, 4]) == False
